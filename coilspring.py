0131b9f92be0399ace0f7472055136b002e28d8f
Cameron Storer <cameronstorer0@gmail.com>
2024-04-03 16:01:09 -0500

    here it is boi

--- coilspring.py
+++ coilspring.py
@@ -1,7 +1,7 @@
 from random import choice
 letters = ["a","b","c","d","e","f","g"]
 
-def AIDS(how):
+def BROTHER(how):
     out = []
     x = 0
     while(x<how):
@@ -9,4 +9,4 @@ def AIDS(how):
         x+= 1
     return out
 
-print(AIDS(50))
+print(BROTHER(50))
