7c8e67f2bfb4e8aa70cb67cd03b40dcac4b09cc8
Joe <jbh072@email.latech.edu>
2024-04-08 22:11:34 -0500

    more foundation

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,6 +1,10 @@
 from tkinter import *
+import os
 
 DEBUG = FALSE
+
+WIDTH = 800
+HEIGHT = 600
 
 class CAKE(Frame):
     def __init__(self, master):
@@ -42,11 +46,30 @@ class CAKE(Frame):
             value = value[:11] + '...'
         self._output = value
 
+###################
+window = Tk()
+###################
+
 # Images
-diagram = Image()
+slices = PhotoImage(file = os.path.join('Images'))
+plus = PhotoImage(file = os.path.join('Images'))
+minus = PhotoImage(file = os.path.join('Images'))
+num_slices = PhotoImage(file = os.path.join('Images'))
+status = PhotoImage(file = os.path.join('Images'))
+start = PhotoImage(file = os.path.join('Images'))
+align = PhotoImage(file = os.path.join('Images'))
+e_stop = PhotoImage(file = os.path.join('Images'))
+
+# Buttons
+b_plus = Button(window, image = plus, command = add)
+b_minus = Button(window, image = minus, command = remove)
+b_start = Button(window, image = start, command = begin)
+b_align = Button(window, image = align, command = get_dim)
+b_e_stop = Button(window, image = e_stop, command = exit)
+
 ######################### MAIN ##############################
 
-window = Tk()
+
 
 window.title("C.A.K.E")
 
