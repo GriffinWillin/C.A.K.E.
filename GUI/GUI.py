8a117419a3d1bd2ef5ea354cadf5112255856892
Joe <jbh072@email.latech.edu>
2024-04-08 22:20:08 -0500

    updated foundation

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -26,10 +26,10 @@ class CAKE(Frame):
 	# Mutator with range checking to control length of input
     @input.setter
     def input(self, value):
-        if (len(value) <= 14):
-            value = value
-        else:
-            value = value[:14]
+        if (value <= 1):
+            value = 2
+        elif (value > 8):
+            value = 8
         self._input = value
 		
 	# Accessor
@@ -40,39 +40,53 @@ class CAKE(Frame):
 	# Mutator with range checking to control length of output
     @output.setter
     def output(self, value):
-        if (len(value) <= 14):
-            value = value
-        else:
-            value = value[:11] + '...'
+        if (value <= 1):
+            value = 2
+        elif (value > 8):
+            value = 8
         self._output = value
+
+    # Functions
+    def add(self):
+        pass
+    
+    def remove(self):
+        pass
+
+    def begin(self):
+        pass
+
+    def get_dim(self):
+        pass
 
 ###################
 window = Tk()
+window.title("C.A.K.E")
+app = CAKE(window)
 ###################
 
 # Images
 slices = PhotoImage(file = os.path.join('Images'))
 plus = PhotoImage(file = os.path.join('Images'))
 minus = PhotoImage(file = os.path.join('Images'))
-num_slices = PhotoImage(file = os.path.join('Images'))
+# num_slices = PhotoImage(file = os.path.join('Images'))
 status = PhotoImage(file = os.path.join('Images'))
 start = PhotoImage(file = os.path.join('Images'))
 align = PhotoImage(file = os.path.join('Images'))
 e_stop = PhotoImage(file = os.path.join('Images'))
 
+# Labels
+diagram = Label(window, image = slices)
+status = Label(window, image = status)
+num = Label(window, text = "NUMBER OF SLICES")
+
 # Buttons
-b_plus = Button(window, image = plus, command = add)
-b_minus = Button(window, image = minus, command = remove)
-b_start = Button(window, image = start, command = begin)
-b_align = Button(window, image = align, command = get_dim)
+b_plus = Button(window, image = plus, command = CAKE.add)
+b_minus = Button(window, image = minus, command = CAKE.remove)
+b_start = Button(window, image = start, command = CAKE.begin)
+b_align = Button(window, image = align, command = CAKE.get_dim)
 b_e_stop = Button(window, image = e_stop, command = exit)
 
 ######################### MAIN ##############################
-
-
-
-window.title("C.A.K.E")
-
-app = CAKE(window)
 
 window.mainloop()
